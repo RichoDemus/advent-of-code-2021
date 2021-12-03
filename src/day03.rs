bc7bc878ae60a3ede08cdcff03490fc075ab4c28
RichoDemus <git@richodemus.com>
2021-12-03 20:18:41 +0100

    Implement bitwise solution and do some cleaning

--- src/day03.rs
+++ src/day03.rs
@@ -48,57 +48,81 @@ fn part1(input: &[Vec<u8>]) -> u64 {
     gamma_rate2 * epsilon_rate2
 }
 
+#[aoc(day3, part1, bit)]
+fn part1_bit(input: &[Vec<u8>]) -> u64 {
+    let length = input[0].len();
+    let mut gamma_rate = 0;
+    let mut epsilon_rate = 0;
+
+    for column in 0..length {
+        let mut zeroes = 0;
+        let mut ones = 0;
+        for line in input {
+            let bit = line[column];
+            match bit {
+                0 => zeroes += 1,
+                1 => ones += 1,
+                _ => panic!("wat"),
+            }
+        }
+        // left shift both rates
+        gamma_rate <<= 1;
+        epsilon_rate <<= 1;
+        if zeroes > ones {
+            gamma_rate |= 0; // set rightmost bit to 0
+            epsilon_rate |= 1; // set rightmost bit to 1
+        } else {
+            gamma_rate |= 1;
+            epsilon_rate |= 0;
+        }
+    }
+    gamma_rate * epsilon_rate
+}
+
 #[aoc(day3, part2)]
 fn part2(input: &[Vec<u8>]) -> u64 {
-    oxygen_generator_rating(input) * co2_scrubber_rating(input)
+    let oxygen_rating = calculate_rating(input, |zeroes, ones| {
+        if ones.len() < zeroes.len() {
+            zeroes
+        } else {
+            ones
+        }
+    });
+
+    let co2_scrubber_rating = calculate_rating(input, |zeroes, ones| {
+        if ones.len() < zeroes.len() {
+            ones
+        } else {
+            zeroes
+        }
+    });
+    oxygen_rating * co2_scrubber_rating
 }
 
-fn oxygen_generator_rating(report: &[Vec<u8>]) -> u64 {
+type ReportDeciderFn = fn(Vec<Vec<u8>>, Vec<Vec<u8>>) -> Vec<Vec<u8>>;
+fn calculate_rating(report: &[Vec<u8>], report_decider: ReportDeciderFn) -> u64 {
     let mut report: Vec<Vec<u8>> = report.to_vec();
     for column in 0..12 {
         let (ones, zeroes) = report.into_iter().split(|i| i[column] == 0);
         let zeroes = zeroes.collect::<Vec<_>>();
         let ones = ones.collect::<Vec<_>>();
 
-        report = if ones.len() < zeroes.len() {
-            zeroes
-        } else {
-            ones
-        };
+        report = report_decider(zeroes, ones);
+
         if report.len() == 1 {
-            let stuff_str: String = report[0]
-                .iter()
-                .map(std::string::ToString::to_string)
-                .collect::<String>();
-            let result = u64::from_str_radix(stuff_str.as_str(), 2).unwrap();
-            return result;
+            return array_of_binary_to_decimal(&report[0]);
         }
     }
     panic!("Unreachable code")
 }
 
-fn co2_scrubber_rating(report: &[Vec<u8>]) -> u64 {
-    let mut report: Vec<Vec<u8>> = report.to_vec();
-    for column in 0..12 {
-        let (ones, zeroes) = report.into_iter().split(|i| i[column] == 0);
-        let zeroes = zeroes.collect::<Vec<_>>();
-        let ones = ones.collect::<Vec<_>>();
-
-        report = if ones.len() < zeroes.len() {
-            ones
-        } else {
-            zeroes
-        };
-        if report.len() == 1 {
-            let stuff_str: String = report[0]
-                .iter()
-                .map(std::string::ToString::to_string)
-                .collect::<String>();
-            let result = u64::from_str_radix(stuff_str.as_str(), 2).unwrap();
-            return result;
-        }
-    }
-    panic!("Unreachable code")
+fn array_of_binary_to_decimal(array: &[u8]) -> u64 {
+    let str: String = array
+        .iter()
+        .map(std::string::ToString::to_string)
+        .collect::<String>();
+    let result = u64::from_str_radix(str.as_str(), 2).unwrap();
+    result
 }
 
 #[cfg(test)]
@@ -110,6 +134,7 @@ mod tests {
         let input = include_str!("../input/2021/day3.txt");
         let input = parse_input(input);
         assert_eq!(part1(input.as_slice()), 2724524);
+        assert_eq!(part1_bit(input.as_slice()), 2724524);
     }
 
     #[test]
@@ -154,13 +179,6 @@ mod tests {
 00010
 01010"#,
         );
-
-        assert_eq!(
-            oxygen_generator_rating(&input),
-            23,
-            "wrong oxygen generator rating"
-        );
-        assert_eq!(co2_scrubber_rating(&input), 10, "wrong co2 scrubber rating");
 
         let result = part2(&input);
 
